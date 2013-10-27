99af502e6a65bad5035cf17761daafb16cac5b0b
Emilio Duarte <eduarte2205@gmail.com>
2013-10-27 11:08:08 -0600

    Changes in Articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ActiveRecord::Base
+	validates :name, presence: true, uniqueness: true
+	validates :description, presence: true
+	validates :price, presence: true
+	validates :total_in_shelf, presence: true
+	validates :total_in_vault, presence: true
+	validates :store_id, presence: true
 	belongs_to :store
 end
